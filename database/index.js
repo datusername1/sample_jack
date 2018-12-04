25156aa53c553fb97e9d11266f2241a6c2255b3a
tim <timothy.tran835@gmail.com>
2018-12-04 15:50:59 -0800

    ready files for docker ec2

--- database/index.js
+++ database/index.js
@@ -1,15 +1,10 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const db = new Sequelize(
-  process.env.DB_DATABASE,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: 'productionPG',
-    dialect: 'postgres',
-  }
-);
+const db = new Sequelize('jackrealestate', 'postgres', 'timtran', {
+  host: 'productionPG',
+  dialect: 'postgres',
+});
 
 db.authenticate()
   .then(() => {
